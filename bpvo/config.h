648eee0dea4f31b5bd58da4c4827f054e968a39b
Hatem Alismail <halismai@cs.cmu.edu>
2016-03-02 18:49:48 -0500

    config

--- bpvo/config.h
+++ bpvo/config.h
@@ -21,8 +21,8 @@
 /** DO NOT EDIT auto generated header */
 #define BPVO_VERSION_MAJOR "0"
 #define BPVO_VERSION_MINOR "9"
-#define BPVO_VERSION_PATCH "3-v0.9.3-33-ge194e20-RelWithDebInfo"
-#define BPVO_BUILD_DATE    "Wed Mar  2 18:00:40 EST 2016"
+#define BPVO_VERSION_PATCH "3-v0.9.3-35-g85c4082-RelWithDebInfo"
+#define BPVO_BUILD_DATE    "Wed Mar  2 18:49:08 EST 2016"
 #define BPVO_BUILD_STR \
     "BPVO version: " BPVO_VERSION_MAJOR "." BPVO_VERSION_MINOR "." BPVO_VERSION_PATCH \
     "\nBuilt on: " BPVO_BUILD_DATE
